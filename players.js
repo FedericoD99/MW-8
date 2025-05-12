409eddd7428f402f1c57513a9f267e4f6351569c
FedericoD99 <526FDiPalma@frhsd.com>
2025-05-12 10:43:17 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,43 @@
+const players = [
+    {
+        firstName:"Aaron",
+        lastName:"Judge",
+        position:"RF",
+        age:33,
+        photo: 'https://piscum.photos/200/300'
+
+    },
+    {
+        firstName:"Aaron",
+        lastName:"Judge",
+        position:"RF",
+        age:33,
+        photo: 'https://piscum.photos/200/300'
+
+    },
+    {
+        firstName:"Aaron",
+        lastName:"Judge",
+        position:"RF",
+        age:33,
+        photo: 'https://piscum.photos/200/300'
+
+    },
+    {
+        firstName:"Aaron",
+        lastName:"Judge",
+        position:"RF",
+        age:33,
+        photo: 'https://piscum.photos/200/300'
+
+    },
+    {
+        firstName:"Aaron",
+        lastName:"Judge",
+        position:"RF",
+        age:33,
+        photo: 'https://piscum.photos/200/300'
+
+    }
+
+]
