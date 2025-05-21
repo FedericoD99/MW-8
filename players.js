5187045df56d1772b0b171510d5b7df35027b7b7
FedericoD99 <526FDiPalma@frhsd.com>
2025-05-21 21:10:26 +0000

    DID A LOT, added the drop down on roster page, added about page, fixed footer, and got the colors for the player positions, and tried the fun fact using the "this"

--- players.js
+++ players.js
@@ -1,215 +1,241 @@
 const players = [
     {
-        firstName: 'Aaron',
-        lastName: 'Judge',
-        position: 'RF',
-        age: 33,
-        photo: 'imgs/judge.png'
+        firstName: 'Aaron', 
+        lastName: 'Judge', 
+        position: 'RF', 
+        age: 33, 
+        photo: 'imgs/judge.png', 
+        funFact: 'Hit 62 home runs in 2022, breaking the AL record.'
     },
     {
-        firstName: 'Cody',
-        lastName: 'Bellinger',
-        position: '1B',
-        age: 29,
-        photo: 'imgs/bellinger.png'
+        firstName: 'Cody', 
+        lastName: 'Bellinger', 
+        position: '1B', 
+        age: 29, 
+        photo: 'imgs/bellinger.png', 
+        funFact: 'Won the NL MVP in 2019 with the Dodgers.'
     },
     {
-        firstName: 'Jasson',
-        lastName: 'Dominguez',
-        position: 'LF',
-        age: 22,
-        photo: 'imgs/jasond.png'
+    firstName: 'Jasson',
+    lastName: 'Dominguez',
+    position: 'CF',
+    age: 22,
+    photo: 'imgs/jasond.png',
+    funFact: 'Nicknamed The Martian for his out-of-this-world talent.'
     },
     {
-        firstName: 'Trent',
-        lastName: 'Grisham',
-        position: 'CF',
-        age: 28,
-        photo: 'imgs/grisham.png'
+        firstName: 'Trent', 
+        lastName: 'Grisham', 
+        position: 'CF', 
+        age: 28, 
+        photo: 'imgs/grisham.png', 
+        funFact: 'Known for his elite defensive skills in center field.'
     },
     {
-        firstName: 'Anthony',
-        lastName: 'Volpe',
-        position: 'SS',
-        age: 24,
-        photo: 'imgs/volpe.png'
-    },
-     {
-        firstName: 'Jorbit',
-        lastName: 'Vivas',
-        position: '2B',
-        age: 24,
-        photo: 'imgs/vivas.png'
+        firstName: 'Anthony', 
+        lastName: 'Volpe', 
+        position: 'SS', 
+        age: 24, 
+        photo: 'imgs/volpe.png', 
+        funFact: 'First Yankee to win a Gold Glove at shortstop.'
     },
     {
-        firstName: 'Ben',
-        lastName: 'Rice',
-        position: 'DH',
-        age: 26,
-        photo: 'imgs/rice.png'
+        firstName: 'Jorbit', 
+        lastName: 'Vivas', 
+        position: '2B', 
+        age: 24, 
+        photo: 'imgs/vivas.png', 
+        funFact: 'Acquired in a trade from the Dodgers in 2023.'
     },
     {
-        firstName: 'Pablo',
-        lastName: 'Reyes',
-        position: 'SS',
-        age: 31,
-        photo: 'imgs/reyes.png'
+        firstName: 'Ben', 
+        lastName: 'Rice', 
+        position: 'DH', 
+        age: 26, 
+        photo: 'imgs/rice.png', 
+        funFact: 'Drafted by the Yankees in the 12th round in 2021.'
     },
     {
-        firstName: 'Oswald',
-        lastName: 'Peraza',
-        position: '2B',
-        age: 24,
-        photo: 'imgs/peraza.png'
-    },
-     {
-        firstName: 'DJ',
-        lastName: 'LeMahieu',
-        position: '1B',
-        age: 36,
-        photo: 'imgs/dj.png'
+        firstName: 'Pablo', 
+        lastName: 'Reyes', 
+        position: 'SS', 
+        age: 31, 
+        photo: 'imgs/reyes.png', 
+        funFact: 'Played for three different MLB teams before joining the Yankees.'
     },
     {
-        firstName: 'Paul',
-        lastName: 'Goldschmidt',
-        position: '1B',
-        age: 37,
-        photo: 'imgs/goldshmidt.png'
+        firstName: 'Oswald', 
+        lastName: 'Peraza', 
+        position: '2B', 
+        age: 24, 
+        photo: 'imgs/peraza.png', 
+        funFact: 'Made his MLB debut in 2022 at age 22.'
     },
     {
-        firstName: 'Jazz',
-        lastName: 'Chisholm Jr.',
-        position: '2B',
-        age: 27,
-        photo: 'imgs/jazz.png'
+        firstName: 'DJ', 
+        lastName: 'LeMahieu', 
+        position: '1B', 
+        age: 36, 
+        photo: 'imgs/dj.png', 
+        funFact: 'Won batting titles in both the NL and AL.'
     },
     {
-        firstName: 'Oswaldo',
-        lastName: 'Cabrera',
-        position: '3B',
-        age: 26,
-        photo: 'imgs/cabrera.png'
-    },
-     {
-        firstName: 'Austin',
-        lastName: 'Wells',
-        position: 'C',
-        age: 25,
-        photo: 'imgs/wells.png'
+        firstName: 'Paul', 
+        lastName: 'Goldschmidt', 
+        position: '1B', 
+        age: 37, 
+        photo: 'imgs/goldshmidt.png', 
+        funFact: 'Won the NL MVP in 2022 with the Cardinals.'
     },
     {
-        firstName: 'J.C.',
-        lastName: 'Escarra',
-        position: 'C',
-        age: 30,
-        photo: 'imgs/escarra.png'
+        firstName: 'Jazz', 
+        lastName: 'Chisholm Jr.', 
+        position: '2B', 
+        age: 27, 
+        photo: 'imgs/jazz.png', 
+        funFact: 'Known for his vibrant personality and colorful cleats.'
     },
     {
-        firstName: 'Ryan',
-        lastName: 'Yarbrough',
-        position: 'RP',
-        age: 33,
-        photo: 'imgs/yarbrough.png'
+        firstName: 'Oswaldo', 
+        lastName: 'Cabrera', 
+        position: '3B', 
+        age: 26, 
+        photo: 'imgs/cabrera.png', funFact: 'Plays multiple positions, including outfield and infield.'
     },
     {
-        firstName: 'Devin',
-        lastName: 'Williams',
-        position: 'RP',
-        age: 30,
-        photo: 'imgs/williams.png'
-    },
-     {
-        firstName: 'Luke',
-        lastName: 'Weaver',
-        position: 'RP',
-        age: 31,
-        photo: 'imgs/weaver.png'
+        firstName: 'Austin', 
+        lastName: 'Wells', 
+        position: 'C', 
+        age: 25, 
+        photo: 'imgs/wells.png', 
+        funFact: 'Drafted in the first round by the Yankees in 2020.'
     },
     {
-        firstName: 'Will',
-        lastName: 'Warren',
-        position: 'P',
-        age: 25,
-        photo: 'imgs/warren.png'
+        firstName: 'J.C.', 
+        lastName: 'Escarra', 
+        position: 'C', 
+        age: 30, 
+        photo: 'imgs/escarra.png', 
+        funFact: 'Signed as a minor league free agent in 2024.'
     },
     {
-        firstName: 'Marcus',
-        lastName: 'Stroman',
-        position: 'P',
-        age: 34,
-        photo: 'imgs/stroman.png'
+        firstName: 'Ryan', 
+        lastName: 'Yarbrough', 
+        position: 'RP', 
+        age: 33, 
+        photo: 'imgs/yarbrough.png', 
+        funFact: 'Known for his slow curveball and versatility.'
     },
     {
-        firstName: 'Clarke',
-        lastName: 'Schmidt',
-        position: 'P',
-        age: 29,
-        photo: 'imgs/clarke.png'
-    },
-     {
-        firstName: 'Carlos',
-        lastName: 'Rodon',
-        position: 'P',
-        age: 32,
-        photo: 'imgs/rodon.png'
+        firstName: 'Devin', 
+        lastName: 'Williams', 
+        position: 'RP', 
+        age: 30, 
+        photo: 'imgs/williams.png', 
+        funFact: 'Won the NL Reliever of the Year in 2020.'
     },
     {
-        firstName: 'Mark',
-        lastName: 'Leiter Jr.',
-        position: 'P',
-        age: 34,
-        photo: 'imgs/leiter.png'
+        firstName: 'Luke', 
+        lastName: 'Weaver', 
+        position: 'RP', 
+        age: 31, 
+        photo: 'imgs/weaver.png', 
+        funFact: 'Pitched in the 2023 ALCS for the Yankees.'
     },
     {
-        firstName: 'Tim',
-        lastName: 'Hill',
-        position: 'P',
-        age: 35,
-        photo: 'imgs/hill.png'
+        firstName: 'Will', 
+        lastName: 'Warren', 
+        position: 'P', 
+        age: 25, 
+        photo: 'imgs/warren.png', 
+        funFact: 'Top pitching prospect in the Yankees system.'
     },
     {
-        firstName: 'Ian',
-        lastName: 'Hamilton',
-        position: 'P',
-        age: 29,
-        photo: 'imgs/hamilton.png'
-    },
-     {
-        firstName: 'Max',
-        lastName: 'Fried',
-        position: 'P',
-        age: 31,
-        photo: 'imgs/fried.png'
+        firstName: 'Marcus', 
+        lastName: 'Stroman', 
+        position: 'P', 
+        age: 34, 
+        photo: 'imgs/stroman.png', 
+        funFact: 'Known for his signature "HDMH" motto.'
     },
     {
-        firstName: 'Fernado',
-        lastName: 'Cruz',
-        position: 'P',
-        age: 35,
-        photo: 'imgs/cruz.png'
+        firstName: 'Clarke', 
+        lastName: 'Schmidt', 
+        position: 'P', 
+        age: 29, 
+        photo: 'imgs/clarke.png', 
+        funFact: 'Made his MLB debut in the 2020 postseason.'
     },
     {
-        firstName: 'Giancarlo',
-        lastName: 'Stanton',
-        position: 'DH',
-        age: 35,
-        photo: 'imgs/stanton.png'
+        firstName: 'Carlos', 
+        lastName: 'Rodon', 
+        position: 'P', 
+        age: 32, 
+        photo: 'imgs/rodon.png', 
+        funFact: 'Struck out 422 batters in 2021-2022 combined.'
     },
     {
-        firstName: 'Gerrit',
-        lastName: 'Cole',
-        position: 'SP',
-        age: 34,
-        photo: 'imgs/cole.png'
+        firstName: 'Mark', 
+        lastName: 'Leiter Jr.', 
+        position: 'P', 
+        age: 34, 
+        photo: 'imgs/leiter.png', 
+        funFact: 'Son of former MLB pitcher Mark Leiter.'
     },
     {
-        firstName: 'Luis',
-        lastName: 'Gil',
-        position: 'SP',
-        age: 26,
-        photo: 'imgs/gil.png'
+        firstName: 'Tim', 
+        lastName: 'Hill', 
+        position: 'P', 
+        age: 35, 
+        photo: 'imgs/hill.png', 
+        funFact: 'Known for his sidearm pitching style.'
     },
-   
-    
-
-]
+    {
+        firstName: 'Ian', 
+        lastName: 'Hamilton', 
+        position: 'P', 
+        age: 29, 
+        photo: 'imgs/hamilton.png', 
+        funFact: 'Acquired by the Yankees in 2023 via trade.'
+    },
+    {
+        firstName: 'Max', 
+        lastName: 'Fried', 
+        position: 'P', 
+        age: 31, 
+        photo: 'imgs/fried.png', 
+        funFact: 'Won a World Series with the Braves in 2021.'
+    },
+    {
+        firstName: 'Fernado', 
+        lastName: 'Cruz', 
+        position: 'P', 
+        age: 35, 
+        photo: 'imgs/cruz.png', 
+        funFact: 'Signed with the Yankees as a free agent in 2024.'
+    },
+    {
+        firstName: 'Giancarlo', 
+        lastName: 'Stanton', 
+        position: 'DH', 
+        age: 35, 
+        photo: 'imgs/stanton.png', 
+        funFact: 'Hit 59 home runs in 2017 with the Marlins.'
+    },
+    {
+        firstName: 'Gerrit', 
+        lastName: 'Cole', 
+        position: 'SP', 
+        age: 34, 
+        photo: 'imgs/cole.png', 
+        funFact: 'Won the AL Cy Young Award in 2023.'
+    },
+    {
+        firstName: 'Luis', 
+        lastName: 'Gil', 
+        position: 'SP', 
+        age: 26, 
+        photo: 'imgs/gil.png', 
+        funFact: 'Set a Yankees rookie record for strikeouts in 2024.'
+    }
+];
--- roster.js
+++ roster.js
@@ -1,23 +1,138 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.getElementById('rosterGrid');
+    const searchInput = document.getElementById('searchInput');
+    const positionFilter = document.getElementById('positionFilter');
+    const clearButton = document.getElementById('clearSearch');
+
+    // Dynamically populate position dropdown
+    const positions = players
+        .map(p => p.position)
+        .reduce((unique, pos) => unique.includes(pos) ? unique : [...unique, pos], [])
+        .sort();
+    positionFilter.innerHTML = '<option value="all">All Positions</option>';
+    positions.forEach(pos => {
+        const option = document.createElement('option');
+        option.value = pos;
+        option.textContent = pos;
+        positionFilter.appendChild(option);
+    });
+
     const render = list => {
         grid.innerHTML = '';
         list.forEach(p => {
+            const modalId = `playerModal-${p.firstName}-${p.lastName}`.replace(/\s+/g, '-');
+            const escapedFunFact = p.funFact.replace(/"/g, '&quot;').replace(/'/g, '&apos;');
             const col = document.createElement('div');
-            col.className = 'col-6 col-lg-2';
-            col.innerHTML = 
-            `
+            col.className = 'col-6 col-md-4 col-lg-2';
+            col.innerHTML = `
                 <div class="card h-100 shadow-sm">
                     <img src="${p.photo}" alt="${p.firstName} ${p.lastName}" class="card-img-top">
                     <div class="card-body text-center">
                         <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
-                        <div class='badge badge-position badge-pos-${p.position}'>${p.position}</div>
-                        <p class="small text-muted mb-0">Age ${p.age}</p>
+                        <div class="badge badge-position position-${p.position}">${p.position}</div>
+                        <p class="small text-muted mb-2">Age ${p.age}</p>
+                        <button class="btn btn-sm btn-outline-primary toggle-fact" data-fact="${escapedFunFact}" data-modal-id="${modalId}">Fun Fact</button>
+                    </div>
+                </div>
+                <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="${modalId}Label" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="${modalId}Label">${p.firstName} ${p.lastName}</h5>
+                                <button type="button" class="btn-close" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body"></div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-close-modal>Close</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             `;
             grid.appendChild(col);
         });
+
+        // Attach event listeners for modals using 'this'
+        document.querySelectorAll('.toggle-fact').forEach(button => {
+            button.addEventListener('click', function() {
+                const modalId = this.getAttribute('data-modal-id');
+                const funFact = this.getAttribute('data-fact');
+                const modal = document.getElementById(modalId);
+                const modalBody = modal.querySelector('.modal-body');
+                modalBody.textContent = funFact;
+                modal.classList.add('show');
+                modal.style.display = 'block';
+                document.body.classList.add('modal-open');
+                const backdrop = document.createElement('div');
+                backdrop.className = 'modal-backdrop fade show';
+                document.body.appendChild(backdrop);
+            });
+        });
+
+        // Closes modal
+        document.querySelectorAll('.btn-close, [data-close-modal]').forEach(btn => {
+            btn.addEventListener('click', function() {
+                const modal = this.closest('.modal');
+                modal.classList.remove('show');
+                modal.style.display = 'none';
+                document.body.classList.remove('modal-open');
+                const backdrop = document.querySelector('.modal-backdrop');
+                if (backdrop) backdrop.remove();
+            });
+        });
+
+        // Close modal when clicking outside
+        document.querySelectorAll('.modal').forEach(modal => {
+            modal.addEventListener('click', function(e) {
+                if (e.target === this) {
+                    this.classList.remove('show');
+                    this.style.display = 'none';
+                    document.body.classList.remove('modal-open');
+                    const backdrop = document.querySelector('.modal-backdrop');
+                    if (backdrop) backdrop.remove();
+                }
+            });
+        });
     };
+
+    const filterPlayers = () => {
+        const searchTerm = searchInput.value.trim().toLowerCase();
+        const position = positionFilter.value;
+
+        let filteredPlayers = players;
+
+        // Filter by position
+        if (position !== 'all') {
+            filteredPlayers = filteredPlayers.filter(p => p.position === position);
+        }
+
+        // Filter by name or age
+        if (searchTerm) {
+            filteredPlayers = filteredPlayers.filter(p => {
+                const fullName = `${p.firstName} ${p.lastName}`.toLowerCase();
+                if (searchTerm.includes('-')) {
+                    const [minAge, maxAge] = searchTerm.split('-').map(Number);
+                    return p.age >= minAge && p.age <= maxAge;
+                } else if (!isNaN(searchTerm)) {
+                    return p.age === Number(searchTerm);
+                } else {
+                    return fullName.includes(searchTerm);
+                }
+            });
+        }
+
+        render(filteredPlayers);
+    };
+
+    // Event listeners for search and filter
+    searchInput.addEventListener('input', filterPlayers);
+    positionFilter.addEventListener('change', filterPlayers);
+    clearButton.addEventListener('click', () => {
+        searchInput.value = '';
+        positionFilter.value = 'all';
+        render(players);
+    });
+
+    // Initial render
     render(players);
 });
